fbc9ba9509a5387d7c02d573f1534228c56fe0e5
occupytheroot <tantien.hmtt@gmail.com>
2023-11-02 17:00:43 +0700

    11. syntax ok , but post method error, can't login

---
+++ backend/src/constants/httpStatus.js
@@ -0,0 +1,2 @@
+// file use record the name of http status instead of number 
+export const BAD_REQUEST = 400 ; 
---
+++ backend/src/routers/user.router.js
@@ -0,0 +1,48 @@
+import { Router } from 'express'
+import { sample_users } from '../data.js';
+import jwt from "jsonwebtoken"
+import { BAD_REQUEST } from '../constants/httpStatus.js';
+
+const router = Router() ; 
+
+router.post ('/login', (req,res) => {
+    const { email, password } = req.body;
+    const user = sample_users.find(
+        user => user.email === email && user.password === password  
+    ); 
+
+
+
+    if(user){
+        res.send(generateTokenResponse(user)); 
+        return; 
+    }
+
+    res.status(BAD_REQUEST).send("User or password is invalid");
+}); 
+
+const generateTokenResponse = user => {
+    const token = jwt.sign(
+        {
+            id: user.id, 
+            email: user.email, 
+            isAdmin: user.isAdmin,
+        }, 
+        //key , but in now, use random text 
+        'randomtext', 
+        {
+            expiresIn: '1d', 
+        },
+    );
+
+    return {
+        id: user.id, 
+        emai: user.email, 
+        name: user.name, 
+        address: user.address, 
+        isAdmin: user.isAdmin, 
+        token,
+    }
+};
+
+export default router;
