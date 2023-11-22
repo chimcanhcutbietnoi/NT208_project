ee141d6d21066288c2ec856b5f129dd7c853dccd
occupytheroot <tantien.hmtt@gmail.com>
2023-11-22 22:24:29 +0700

    16: done

--- backend/src/middleware/auth.mid.js
+++ backend/src/middleware/auth.mid.js
@@ -3,7 +3,6 @@ const {verify} = pkg;
 import { UNAUTHORIZED } from '../constants/httpStatus.js';
 
 export default (req, res, next) => {
-  console.log("middle ware acitve");
   const token = req.headers.access_token;
   if (!token) return res.status(UNAUTHORIZED).send();
 
