2ba9e1d9429ea1877049b04f407874f17ec8d6fc
bbssmile <103357333+bbssmile@users.noreply.github.com>
2023-11-29 11:35:47 +0700

    20 public

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,14 @@ import userRouter from './routers/user.router.js'
 import dotenv from 'dotenv'
 import { dbconnect } from './config/database.config.js';
 import oderRouter from './routers/order.router.js'
-
+import {fileURLToPath} from 'url';
+import path, { dirname } from 'path';
 dotenv.config();
 dbconnect();
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname =  dirname(__fileName);
+
 const app = express(); 
 app.use(express.json()); // app use file json  to analyze 
 app.use(
@@ -24,6 +29,13 @@ app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter); 
 
 app.use('/api/orders', oderRouter);
+
+const publicFolder = path.join(__dirname,'public');
+app.use(express.static(publicFolder));
+app.get('*', (req, res) => {
+    const indexFilePath = path.join(publicFolder, 'index.html');
+    res.sendFile(indexFilePath);
+});
 
 const PORT = 5000; 
 app.listen(PORT, () => {
